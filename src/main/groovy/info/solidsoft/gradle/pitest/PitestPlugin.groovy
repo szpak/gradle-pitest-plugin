bd8f8022a888beb4aff6d964cfcefc91b346922b
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2013-05-08 01:08:04 +0200

    upgrade PIT to 0.30 final

--- src/main/groovy/info/solidsoft/gradle/pitest/PitestPlugin.groovy
+++ src/main/groovy/info/solidsoft/gradle/pitest/PitestPlugin.groovy
@@ -29,7 +29,7 @@ import com.google.common.annotations.VisibleForTesting
  * The main class for Pitest plugin.
  */
 class PitestPlugin implements Plugin<Project> {
-    final static DEFAULT_PITEST_VERSION = '0.30-SNAPSHOT'
+    final static DEFAULT_PITEST_VERSION = '0.30'
     final static PITEST_TASK_GROUP = "Report"
     final static PITEST_TASK_NAME = "pitest"
     final static PITEST_CONFIGURATION_NAME = 'pitest'
