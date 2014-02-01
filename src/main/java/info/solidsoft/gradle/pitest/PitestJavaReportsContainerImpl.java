f66472c95ce2a6e5264a08931a391c0b95bf0aa9
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2014-02-01 16:25:54 +0100

    Add ReportContainer in Java
    It compiles fine

---
+++ src/main/java/info/solidsoft/gradle/pitest/PitestJavaReportsContainerImpl.java
@@ -0,0 +1,38 @@
+package info.solidsoft.gradle.pitest;
+
+import org.gradle.api.Task;
+import org.gradle.api.reporting.ConfigurableReport;
+import org.gradle.api.reporting.DirectoryReport;
+import org.gradle.api.reporting.Report;
+import org.gradle.api.reporting.SingleFileReport;
+import org.gradle.api.reporting.internal.TaskGeneratedSingleDirectoryReport;
+import org.gradle.api.reporting.internal.TaskGeneratedSingleFileReport;
+import org.gradle.api.reporting.internal.TaskReportContainer;
+
+/**
+ * @author Marcin Zajączkowski, 2013-12-16
+ */
+public class PitestJavaReportsContainerImpl extends TaskReportContainer<Report> implements PitestJavaReportsContainer {
+
+    public PitestJavaReportsContainerImpl(Task task) {
+        super(ConfigurableReport.class, task);
+        add(TaskGeneratedSingleDirectoryReport.class, "html", task, "index.html");
+        add(TaskGeneratedSingleFileReport.class, "xml", task);
+        add(TaskGeneratedSingleFileReport.class, "csv", task);
+    }
+
+    @Override
+    public DirectoryReport getHtml() {
+        return (DirectoryReport)getByName("html");
+    }
+
+    @Override
+    public SingleFileReport getXml() {
+        return (SingleFileReport)getByName("xml");
+    }
+
+    @Override
+    public SingleFileReport getCsv() {
+        return (SingleFileReport)getByName("csv");
+    }
+}
