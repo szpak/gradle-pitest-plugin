a5a6eba39cd005f094212ebf214aaf56094832fc
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2014-10-06 21:10:15 +0200

    Add simple test for overriding configuration parameter from command line

---
+++ src/funcTest/groovy/info/solidsoft/gradle/pitest/functional/OverridePluginFunctionalSpec.groovy
@@ -0,0 +1,34 @@
+package info.solidsoft.gradle.pitest.functional
+
+import nebula.test.IntegrationSpec
+
+//Note: gradle-override-plugin has important limitations in support for collections and not initialized boolean values
+//See: https://github.com/nebula-plugins/gradle-override-plugin/issues/1 or https://github.com/nebula-plugins/gradle-override-plugin/issues/3
+class OverridePluginFunctionalSpec extends IntegrationSpec {
+
+    def "should allow to override String configuration parameter from command line"() {
+        given:
+            buildFile << """
+                apply plugin: 'java'
+                apply plugin: 'info.solidsoft.pitest'
+                apply plugin: 'nebula-override'
+                group = 'gradle.pitest.test'
+
+                buildscript {
+                    repositories {
+                        mavenCentral()
+                    }
+                    dependencies {
+                        classpath 'com.netflix.nebula:gradle-override-plugin:1.12.+'
+                    }
+                }
+            """.stripIndent()
+        and:
+            writeHelloWorld('gradle.pitest.test.hello')
+        when:
+            def result = runTasksSuccessfully('pitest', '-Doverride.pitest.reportDir=build/treports')
+        then:
+            result.getStandardOutput().contains('Generated 1 mutations Killed 0 (0%)')
+            fileExists('build/treports')
+    }
+}
