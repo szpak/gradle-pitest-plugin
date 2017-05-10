a98462dd9022b6b422f6ec75d88bc9d1d639b4c6
David Burström <davidburstrom@spotify.com>
2017-05-10 21:37:37 +0200

    Add Android application project with flavours

--- src/test/groovy/info/solidsoft/gradle/pitest/AndroidUtils.groovy
+++ src/test/groovy/info/solidsoft/gradle/pitest/AndroidUtils.groovy
@@ -12,7 +12,7 @@ class AndroidUtils {
         }
         def project = builder.build()
         ClassLoader classLoader = AndroidUtils.class.classLoader
-        def resource = classLoader.getResource('AndroidManifest.xml')
+        def resource = classLoader.getResource('lib/AndroidManifest.xml')
         def manifestFile = project.file('src/main/AndroidManifest.xml')
         manifestFile.parentFile.mkdirs()
         manifestFile.write(resource.text)
@@ -26,6 +26,41 @@ class AndroidUtils {
             defaultConfig {
                 minSdkVersion 10
                 targetSdkVersion 25
+            }
+        }
+        project.apply(plugin: "pl.droidsonroids.pitest")
+        return project
+    }
+
+    static createSampleApplicationProject(File... rootDir) {
+        def builder = ProjectBuilder.builder()
+        if (rootDir.length>0){
+            builder.withProjectDir(rootDir[0])
+        }
+        def project = builder.build()
+        ClassLoader classLoader = AndroidUtils.class.classLoader
+        def resource = classLoader.getResource('app/AndroidManifest.xml')
+        def manifestFile = project.file('src/main/AndroidManifest.xml')
+        manifestFile.parentFile.mkdirs()
+        manifestFile.write(resource.text)
+        project.buildscript.repositories {
+            mavenCentral()
+        }
+        project.apply(plugin: "com.android.application")
+        project.android.with {
+            buildToolsVersion '25.0.3'
+            compileSdkVersion 25
+            defaultConfig {
+                minSdkVersion 10
+                targetSdkVersion 25
+            }
+            buildTypes {
+                release { }
+                debug { }
+            }
+            productFlavors {
+                variant1 { }
+                variant2 { }
             }
         }
         project.apply(plugin: "pl.droidsonroids.pitest")
