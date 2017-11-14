672788175ada2e088292b49b143911aa001553f8
Marcin Zajączkowski <szpak@users.sourceforge.net>
2017-11-14 22:34:00 +0100

    Add Gradle up to 4.3.1 to regression test matrix

--- src/funcTest/groovy/info/solidsoft/gradle/pitest/functional/PitestPluginFunctional2Spec.groovy
+++ src/funcTest/groovy/info/solidsoft/gradle/pitest/functional/PitestPluginFunctional2Spec.groovy
@@ -38,7 +38,7 @@ class PitestPluginFunctional2Spec extends AbstractPitestFunctionalSpec {
             result.wasExecuted(':pitest')
             result.getStandardOutput().contains('Generated 1 mutations Killed 1 (100%)')
         where:
-            requestedGradleVersion << resolveRequestedGradleVersions()
+            requestedGradleVersion << resolveRequestedGradleVersions().unique()
     }
 
     //To prevent failure when Spock for Groovy 2.4 is run with Groovy 2.3 delivered with Gradle <2.8
@@ -51,8 +51,8 @@ class PitestPluginFunctional2Spec extends AbstractPitestFunctionalSpec {
     //TODO: Extract regression tests control mechanism to a separate class (or even better trait) when needed in some other place
     private static final String REGRESSION_TESTS_ENV_NAME = "PITEST_REGRESSION_TESTS"
     private static final List<String> GRADLE3_VERSIONS = ["3.5.1", "3.4.1", "3.3", "3.2", "3.1", "3.0"]
-    private static final List<String> GRADLE4_VERSIONS = ["4.1", "4.0.1"]
-    private static final List<String> GRADLE_LATEST_VERSIONS = ["2.14.1", GRADLE3_VERSIONS[0], GRADLE4_VERSIONS[0]]
+    private static final List<String> GRADLE4_VERSIONS = ["4.3.1", "4.2.1", "4.1", "4.0.1"]
+    private static final List<String> GRADLE_LATEST_VERSIONS = ["2.14.1", GRADLE3_VERSIONS.first(), GRADLE4_VERSIONS.first()]
     private static final Range<Integer> GRADLE2_MINOR_RANGE = (14..0)
 
     private static final Closure gradle2AdditionalVersionModifications = { List<String> versions ->
@@ -67,7 +67,7 @@ class PitestPluginFunctional2Spec extends AbstractPitestFunctionalSpec {
             case null:
                 return GRADLE_LATEST_VERSIONS
             case "quick":
-                return GRADLE_LATEST_VERSIONS + "2.0" + "3.0"
+                return GRADLE_LATEST_VERSIONS + "2.0" + GRADLE3_VERSIONS.last() + GRADLE4_VERSIONS.last()
             case "full":
                 return GRADLE4_VERSIONS + GRADLE3_VERSIONS + gradle2AdditionalVersionModifications(GRADLE2_MINOR_RANGE.collect { "2.$it" })
             default:
