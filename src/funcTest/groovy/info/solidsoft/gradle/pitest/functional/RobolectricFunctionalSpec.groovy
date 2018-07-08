a6744634d8d4f3777d2d94bcec6e915eef666b59
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2018-07-09 01:35:18 +0200

    Fix typo in Robolectric test name

---
+++ src/funcTest/groovy/info/solidsoft/gradle/pitest/functional/RobolectricFunctionalSpec.groovy
@@ -0,0 +1,16 @@
+package info.solidsoft.gradle.pitest.functional
+
+import nebula.test.functional.ExecutionResult
+
+class RobolectricFunctionalSpec extends AbstractPitestFunctionalSpec {
+
+    def "should not fail with tests using Robolectric"() {
+        given:
+            copyResources("testProjects/robolectric", "")
+        when:
+            ExecutionResult result = runTasksSuccessfully('pitestRelease')
+        then:
+            result.wasExecuted('pitestRelease')
+            result.getStandardOutput().contains('Generated 3 Killed 0 (0%)')
+    }
+}
