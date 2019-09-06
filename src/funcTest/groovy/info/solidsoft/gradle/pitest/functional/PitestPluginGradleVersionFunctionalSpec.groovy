a367b76c500587d020ba0043e0697ad2e9874f6f
Marcin Zajączkowski <szpak@users.sourceforge.net>
2019-09-06 11:56:25 +0200

    Shrink reproduction of issue with RegularFileProperty
    https://discuss.gradle.org/t/regularpileproperty-vs-property-file/33074/4

--- src/funcTest/groovy/info/solidsoft/gradle/pitest/functional/PitestPluginGradleVersionFunctionalSpec.groovy
+++ src/funcTest/groovy/info/solidsoft/gradle/pitest/functional/PitestPluginGradleVersionFunctionalSpec.groovy
@@ -1,41 +1,22 @@
 package info.solidsoft.gradle.pitest.functional
 
-import com.google.common.base.Predicate
-import com.google.common.base.Predicates
 import groovy.util.logging.Slf4j
+import nebula.test.IntegrationSpec
 import nebula.test.functional.ExecutionResult
-import nebula.test.functional.GradleRunner
-import org.gradle.internal.jvm.Jvm
-import org.gradle.util.GradleVersion
 
-import java.util.regex.Pattern
-
-import static info.solidsoft.gradle.pitest.PitestTaskConfigurationSpec.PIT_PARAMETERS_NAMES_NOT_SET_BY_DEFAULT
-
-/**
- * TODO: Possible extensions:
- *  - Move functional tests to a separate sourceSet and not run them in every build - DONE
- *  - Add nice gradle.build builder
- *  - Add Connector clean up in tear down in IntegrationSpec - DONE
- *  - Add testing against latest nightly Gradle version?
- */
 @Slf4j
 @SuppressWarnings("GrMethodMayBeStatic")
-class PitestPluginGradleVersionFunctionalSpec extends AbstractPitestFunctionalSpec {
-
-    //4.8, but plugin requires 4.9+
-    private static final GradleVersion MINIMAL_SUPPORTED_JAVA12_COMPATIBLE_GRADLE_VERSION = GradleVersion.version("4.9")
-    //https://github.com/gradle/gradle/issues/8681#issuecomment-522951112
-    private static final GradleVersion MINIMAL_SUPPORTED_JAVA13_COMPATIBLE_GRADLE_VERSION = GradleVersion.version("6.0-20190902220030+0000")
+class PitestPluginGradleVersionFunctionalSpec extends IntegrationSpec {
 
     void setup() {
+        fork = true //to make stdout assertion work with Gradle 2.x - http://forums.gradle.org/gradle/topics/unable-to-catch-stdout-stderr-when-using-tooling-api-i-gradle-2-x#reply_15357743
+        memorySafeMode = true   //shutdown Daemon after a few seconds of inactivity
         daemonMaxIdleTimeInSecondsInMemorySafeMode = 1  //trying to mitigate "Gradle killed" issues with Travis
     }
 
     def "should run mutation analysis with Gradle #requestedGradleVersion"() {
         given:
             gradleVersion = requestedGradleVersion
-            classpathFilter = Predicates.and(GradleRunner.CLASSPATH_DEFAULT, FILTER_SPOCK_JAR)
         when:
             copyResources("testProjects/simple1", "")
         then:
@@ -44,66 +25,7 @@ class PitestPluginGradleVersionFunctionalSpec extends AbstractPitestFunctionalSp
             ExecutionResult result = runTasksSuccessfully('pitest')
         then:
             result.wasExecuted(':pitest')
-            result.standardOutput.contains('Generated 1 mutations Killed 1 (100%)')
-        and:    //issue with Gradle <5.0 where Integer/Boolean property had 0/false provided by default
-            //TODO: verifyAll would be great, but it's broken with explicit "assert" - https://github.com/spockframework/spock/issues/855#issuecomment-528411874
-            PIT_PARAMETERS_NAMES_NOT_SET_BY_DEFAULT.each {
-                assert !result.standardOutput.contains("${it}=")
-            }
         where:
             requestedGradleVersion << ["4.10.2", "5.5.1", "5.6.1"]
-    }
-
-    //To prevent failure when Spock for Groovy 2.4 is run with Groovy 2.3 delivered with Gradle <2.8
-    //Spock is not needed in this artificial project - just the test classpath leaks to Gradle instance started by Nebula
-    private static final Pattern SPOCK_JAR_PATTERN = Pattern.compile(".*spock-core-1\\..*.jar")
-    private static final Predicate<URL> FILTER_SPOCK_JAR = { URL url ->
-        return !url.toExternalForm().matches(SPOCK_JAR_PATTERN)
-    } as Predicate<URL>
-
-    //TODO: Extract regression tests control mechanism to a separate class (or even better trait) when needed in some other place
-    private static final String REGRESSION_TESTS_ENV_NAME = "PITEST_REGRESSION_TESTS"
-    private static final List<String> GRADLE4_VERSIONS = ["4.10.2", "4.9"]
-    private static final List<String> GRADLE5_VERSIONS = ["5.6.1", "5.5.1", "5.4.1", "5.3.1", "5.2.1", "5.1.1", "5.0"]
-    private static final List<String> GRADLE6_VERSIONS = [MINIMAL_SUPPORTED_JAVA13_COMPATIBLE_GRADLE_VERSION.version] //for Java 13 compatibility
-    private static final List<String> GRADLE_LATEST_VERSIONS = [GRADLE4_VERSIONS.first(), GRADLE5_VERSIONS.first(), GRADLE6_VERSIONS.first()]
-
-    private List<String> resolveRequestedGradleVersions() {
-        String regressionTestsLevel = System.getenv(REGRESSION_TESTS_ENV_NAME)
-        log.debug("$REGRESSION_TESTS_ENV_NAME set to '${regressionTestsLevel}'")
-        switch (regressionTestsLevel) {
-            case "latestOnly":
-            case null:
-                return GRADLE_LATEST_VERSIONS
-            case "quick":
-                return GRADLE_LATEST_VERSIONS + GRADLE4_VERSIONS.last() + GRADLE5_VERSIONS.last()
-            case "full":
-                return GRADLE5_VERSIONS + GRADLE4_VERSIONS
-            default:
-                log.warn("Unsupported $REGRESSION_TESTS_ENV_NAME value '`$regressionTestsLevel`' (expected 'latestOnly', 'quick' or 'full'). " +
-                        "Assuming 'latestOnly'.")
-                return GRADLE_LATEST_VERSIONS
-        }
-    }
-
-    //Jvm class from Spock doesn't work with Java 9 stable releases - otherwise @IgnoreIf could be used - TODO: check with Spock 1.2
-    private List<String> applyJavaCompatibilityAdjustment(List<String> requestedGradleVersions) {
-        if (!Jvm.current().javaVersion.isJava9Compatible()) {
-            //All supported versions should be Java 8 compatible
-            return requestedGradleVersions
-        }
-        GradleVersion minimalCompatibleGradleVersion = !isJava13Compatible() ? MINIMAL_SUPPORTED_JAVA12_COMPATIBLE_GRADLE_VERSION :
-            MINIMAL_SUPPORTED_JAVA13_COMPATIBLE_GRADLE_VERSION
-        return leaveJavaXCompatibleGradleVersionsOnly(requestedGradleVersions, minimalCompatibleGradleVersion)
-    }
-
-    private List<String> leaveJavaXCompatibleGradleVersionsOnly(List<String> requestedGradleVersions, GradleVersion minimalCompatibleJavaVersion) {
-        List<String> javaXCompatibleGradleVersions = requestedGradleVersions.findAll {
-            GradleVersion.version(it) >= minimalCompatibleJavaVersion
-        }
-        if (javaXCompatibleGradleVersions.size() < 2) {
-            javaXCompatibleGradleVersions.add(minimalCompatibleJavaVersion.version)
-        }
-        return javaXCompatibleGradleVersions
     }
 }
--- src/main/groovy/info/solidsoft/gradle/pitest/PitestPlugin.groovy
+++ src/main/groovy/info/solidsoft/gradle/pitest/PitestPlugin.groovy
@@ -15,173 +15,20 @@
  */
 package info.solidsoft.gradle.pitest
 
-import groovy.transform.CompileStatic
-import groovy.transform.PackageScope
-import org.codehaus.groovy.runtime.DefaultGroovyMethods
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
-import org.gradle.api.artifacts.Configuration
-import org.gradle.api.file.FileCollection
-import org.gradle.api.internal.file.UnionFileCollection
-import org.gradle.api.logging.Logger
-import org.gradle.api.logging.Logging
 import org.gradle.api.plugins.JavaPlugin
-import org.gradle.api.tasks.SourceSet
-import org.gradle.api.tasks.TaskProvider
-
-import static org.gradle.language.base.plugins.LifecycleBasePlugin.VERIFICATION_GROUP
 
 /**
  * The main class for Pitest plugin.
  */
 class PitestPlugin implements Plugin<Project> {
-    public final static String DEFAULT_PITEST_VERSION = '1.4.10'
-    public final static String PITEST_TASK_GROUP = VERIFICATION_GROUP
-    public final static String PITEST_TASK_NAME = "pitest"
-    public final static String PITEST_CONFIGURATION_NAME = 'pitest'
-
-    private final static List<String> DYNAMIC_LIBRARY_EXTENSIONS = ['so', 'dll', 'dylib']
-    private final static List<String> DEFAULT_FILE_EXTENSIONS_TO_FILTER_FROM_CLASSPATH = ['pom'] + DYNAMIC_LIBRARY_EXTENSIONS
-
-    private final static Logger log =  Logging.getLogger(PitestPlugin)
-
-    @PackageScope   //visible for testing
-    final static String PIT_HISTORY_DEFAULT_FILE_NAME = 'pitHistory.txt'
-    private final static String PIT_ADDITIONAL_CLASSPATH_DEFAULT_FILE_NAME = "pitClasspath"
-
-    private Project project
-    private PitestPluginExtension extension
-
     void apply(Project project) {
-        this.project = project
         project.plugins.withType(JavaPlugin).configureEach {
-            createExtensionAndSetDefaultValues()
-            project.tasks.register(PITEST_TASK_NAME, PitestTask) { t ->
-                t.description = "Run PIT analysis for java classes"
-                t.group = PITEST_TASK_GROUP
-                configureTaskDefault(t)
-                t.dependsOn(calculateTasksToDependOn())
-                addPitDependencies(createConfigurations())
-            }
+            PitestPluginExtension extension = project.extensions.create("pitest", PitestPluginExtension, project)
+//            extension.reportDir.set(new File("${project.reporting.baseDir.path}/pitest"))
+            project.tasks.register("pitest", PitestTask)
         }
     }
 
-    private Configuration createConfigurations() {
-        return project.rootProject.buildscript.configurations.maybeCreate(PITEST_CONFIGURATION_NAME).with {
-            visible = false
-            description = "The Pitest libraries to be used for this project."
-            return it
-        }
-    }
-
-    private void createExtensionAndSetDefaultValues() {
-        extension = project.extensions.create("pitest", PitestPluginExtension, project)
-        extension.reportDir.set(new File("${project.reporting.baseDir.path}/pitest"))
-        extension.pitestVersion.set(DEFAULT_PITEST_VERSION)
-        extension.testSourceSets.set([project.sourceSets.test] as Set)
-        extension.mainSourceSets.set([project.sourceSets.main] as Set)
-        extension.fileExtensionsToFilter.set(DEFAULT_FILE_EXTENSIONS_TO_FILTER_FROM_CLASSPATH)
-        extension.useClasspathFile.set(false)
-    }
-
-    private void configureTaskDefault(PitestTask task) {
-
-        task.testPlugin.set(extension.testPlugin)
-        task.reportDir.set(extension.reportDir)
-        task.targetClasses.set(project.providers.provider() {
-                log.debug("Setting targetClasses. project.getGroup: {}, class: {}", project.getGroup(), project.getGroup()?.class)
-                if (extension.targetClasses.isPresent()) {
-                    return extension.targetClasses.get()
-                }
-                if (project.getGroup()) {   //Assuming it is always a String class instance
-                    return [project.getGroup() + ".*"] as Set
-                }
-                return null
-            }
-        )
-        task.targetTests.set(extension.targetTests)
-        task.dependencyDistance.set(extension.dependencyDistance)
-        task.threads.set(extension.threads)
-        task.mutateStaticInits.set(extension.mutateStaticInits)
-        task.includeJarFiles.set(extension.includeJarFiles)
-        task.mutators.set(extension.mutators)
-        task.excludedMethods.set(extension.excludedMethods)
-        task.excludedClasses.set(extension.excludedClasses)
-        task.excludedTestClasses.set(extension.excludedTestClasses)
-        task.avoidCallsTo.set(extension.avoidCallsTo)
-        task.verbose.set(extension.verbose)
-        task.timeoutFactor.set(extension.timeoutFactor)
-        task.timeoutConstInMillis.set(extension.timeoutConstInMillis)
-        task.maxMutationsPerClass.set(extension.maxMutationsPerClass)
-        task.childProcessJvmArgs.set(extension.jvmArgs)
-        task.outputFormats.set(extension.outputFormats)
-        task.failWhenNoMutations.set(extension.failWhenNoMutations)
-        task.includedGroups.set(extension.includedGroups)
-        task.excludedGroups.set(extension.excludedGroups)
-//        task.sourceDirs.set(project.providers.provider() { extension.mainSourceSets*.allSource.srcDirs.flatten() as Set })
-        task.detectInlinedCode.set(extension.detectInlinedCode)
-        task.timestampedReports.set(extension.timestampedReports)
-        task.enableDefaultIncrementalAnalysis.set(extension.enableDefaultIncrementalAnalysis)
-        task.mutationThreshold.set(extension.mutationThreshold)
-        task.coverageThreshold.set(extension.coverageThreshold)
-        task.mutationEngine.set(extension.mutationEngine)
-        task.exportLineCoverage.set(extension.exportLineCoverage)
-//        task.jvmPath.set(extension.jvmPath)
-        task.mainProcessJvmArgs.set(extension.mainProcessJvmArgs)
-//        task.mutableCodePaths.set(extension.additionalMutableCodePaths)
-//        task.pluginConfiguration.set(extension.pluginConfiguration)
-        task.maxSurviving.set(extension.maxSurviving)
-        task.useAdditionalClasspathFile.set(extension.useClasspathFile)
-        task.features.set(extension.features)
-
-        //Temporarily for types not supported in Gradle 4.x
-        task.conventionMapping.with {
-            additionalClasspath = {
-                List<FileCollection> testRuntimeClasspath = extension.testSourceSets.get()*.runtimeClasspath
-
-                FileCollection combinedTaskClasspath = new UnionFileCollection(testRuntimeClasspath)
-                FileCollection filteredCombinedTaskClasspath = combinedTaskClasspath.filter { File file ->
-                    !extension.fileExtensionsToFilter.getOrNull().find { file.name.endsWith(".$it") }
-                }
-
-                return filteredCombinedTaskClasspath
-            }
-            additionalClasspathFile = { new File(project.buildDir, PIT_ADDITIONAL_CLASSPATH_DEFAULT_FILE_NAME) }
-            launchClasspath = {
-                project.rootProject.buildscript.configurations[PITEST_CONFIGURATION_NAME]
-            }
-            mutableCodePaths = { calculateBaseMutableCodePaths() + (extension.additionalMutableCodePaths ?: []) }
-            sourceDirs = { extension.mainSourceSets.get()*.allSource.srcDirs.flatten() as Set }
-            historyInputLocation = { extension.historyInputLocation }
-            historyOutputLocation = { extension.historyOutputLocation }
-            defaultFileForHistoryData = { new File(project.buildDir, PIT_HISTORY_DEFAULT_FILE_NAME) }
-            jvmPath = { extension.jvmPath }
-            pluginConfiguration = { extension.pluginConfiguration }
-        }
-    }
-
-    @CompileStatic
-    private Set<File> calculateBaseMutableCodePaths() {
-        Set<SourceSet> sourceSets = extension.mainSourceSets.get()
-        return sourceSets*.output.classesDirs.files.flatten() as Set<File>
-    }
-
-    @CompileStatic
-    private Set<String> calculateTasksToDependOn() {
-//        //Fails with: NoSuchMethodError: org.codehaus.groovy.runtime.DefaultGroovyMethods.collect(Ljava/lang/Iterable;Lgroovy/lang/Closure;)Ljava/util/List;
-//        //when compiled with Groovy 2.5 (Gradle 5+) and executed with Groovy 2.4 (Gradle <5). Explicit coercion doesn't help.
-//        //TODO: Workaround with DefaultGroovyMethods.collect. Remove once Gradle 4 support is dropped
-//        Set<String> tasksToDependOn = extension.testSourceSets.collect { it.name + "Classes" } as Set
-        Set<SourceSet> testSourceSets = extension.testSourceSets.get()
-        Set<String> tasksToDependOn = DefaultGroovyMethods.collect(testSourceSets) { SourceSet it -> it.name + "Classes" } as Set
-        log.debug("pitest tasksToDependOn: $tasksToDependOn")
-        return tasksToDependOn
-    }
-
-    @CompileStatic
-    private void addPitDependencies(Configuration pitestConfiguration) {
-        log.info("Using PIT: ${extension.pitestVersion.get()}")
-        pitestConfiguration.dependencies.add(project.dependencies.create("org.pitest:pitest-command-line:${extension.pitestVersion.get()}"))
-    }
 }
