846943cfeed5869dffa87c9d033446696556fc7e
Marcin Zajączkowski <szpak@users.sourceforge.net>
2020-02-26 10:37:28 +0100

    [#155] Fix some deprecation warnings related to Gradle 6

--- src/funcTest/groovy/info/solidsoft/gradle/pitest/functional/AcceptanceTestsInSeparateSubprojectFunctionalSpec.groovy
+++ src/funcTest/groovy/info/solidsoft/gradle/pitest/functional/AcceptanceTestsInSeparateSubprojectFunctionalSpec.groovy
@@ -1,17 +1,12 @@
 package info.solidsoft.gradle.pitest.functional
 
 import nebula.test.functional.ExecutionResult
-import spock.util.environment.RestoreSystemProperties
 
 class AcceptanceTestsInSeparateSubprojectFunctionalSpec extends AbstractPitestFunctionalSpec {
 
-    @RestoreSystemProperties
     def "should mutate production code in another subproject"() {
         given:
             copyResources("testProjects/multiproject", "")
-        and:
-            //Until fixed: https://github.com/szpak/gradle-pitest-plugin/issues/155
-            System.setProperty("ignoreDeprecations", "true")
         when:
             ExecutionResult result = runTasksSuccessfully('pitest')
         then:
