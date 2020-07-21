b5e5439e0c4dbb4bc99842c6556e2a4d49f8f363
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2020-07-21 13:55:51 +0200

    Fix PitestPluginTest

--- src/test/groovy/pl/droidsonroids/gradle/pitest/PitestPluginTest.groovy
+++ src/test/groovy/pl/droidsonroids/gradle/pitest/PitestPluginTest.groovy
@@ -19,7 +19,6 @@ import groovy.transform.CompileDynamic
 import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.Task
-import org.gradle.api.file.FileCollection
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
@@ -83,7 +82,7 @@ class PitestPluginTest extends Specification {
         Project project = AndroidUtils.createSampleApplicationProject()
         project.evaluate()
         then:
-        FileCollection classpath = project.tasks["${PitestPlugin.PITEST_TASK_NAME}FreeBlueRelease"].additionalClasspath.files
+        Object classpath = project.tasks["${PitestPlugin.PITEST_TASK_NAME}FreeBlueRelease"].additionalClasspath.files
         assert classpath.find { it.toString().endsWith('sourceFolderJavaResources/freeBlue/release') }
         assert classpath.find { it.toString().endsWith('sourceFolderJavaResources/test/freeBlue/release') }
     }
